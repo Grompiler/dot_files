7f275ae450ebd00ca600f61b47facabe5a2be1de
Pierre Leblond <32479816+Pioterr@users.noreply.github.com>
2022-05-24 01:14:53 +0200

    Simpler dashboard

--- doom.d/config.el
+++ doom.d/config.el
@@ -288,3 +288,19 @@
 (map! :n "C-k" (kbd "Hzz"))
 (map! :n "C-h" (kbd "^"))
 (map! :n "C-l" (kbd "$"))
+
+;; Simpler dashboard
+(defun doom-dashboard-draw-ascii-banner-fn ()
+  (let* ((banner
+          '("                                   E M A C S                                   "))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat
+                 line (make-string (max 0 (- longest-line (length line)))
+                                   32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))

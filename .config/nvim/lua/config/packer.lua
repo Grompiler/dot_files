3cb69e57afc259611d4ac7c4c7f82c608aadf531
Pierre Leblond <32479816+Grompiler@users.noreply.github.com>
2023-05-28 22:54:37 +0200

    Update packer.lua

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -59,4 +59,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use {
+        "windwp/nvim-autopairs",
+    }
 end)

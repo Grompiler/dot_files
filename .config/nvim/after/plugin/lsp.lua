450def48b6c9a2266793332d0f06cd95e53ea7ad
Pierre Leblond <32479816+Grompiler@users.noreply.github.com>
2023-06-06 23:42:07 +0200

    Update lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,9 @@
 -- Install required lsp with :Mason
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero').preset({
+        manage_nvim_cmp = {
+        set_sources = 'recommended'
+    }
+})
 
 lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({ buffer = bufnr })

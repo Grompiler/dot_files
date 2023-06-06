c758dc01c127b2d5e280d11b4b09092a1672c325
Pierre Leblond <32479816+Grompiler@users.noreply.github.com>
2023-06-06 23:43:36 +0200

    Update packer.lua

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -35,6 +35,7 @@ return require('packer').startup(function(use)
 
             -- Autocompletion
             { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-path' },     -- Convenient
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
             { 'L3MON4D3/LuaSnip' },     -- Required
         }

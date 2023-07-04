09c5bf37e669a6162dfbf2a15fa1f05bee4a8f85
Pierre <32479816+Grompiler@users.noreply.github.com>
2023-07-04 22:16:42 +0200

    Update packer.lua

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -40,7 +40,7 @@ return require('packer').startup(function(use)
             { 'L3MON4D3/LuaSnip' },     -- Required
         }
     }
-    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+    use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
     use {
         "nvim-telescope/telescope-file-browser.nvim",
         requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", 'nvim-tree/nvim-web-devicons' }

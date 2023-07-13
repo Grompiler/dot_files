879b2bf3a812f9459f3b0b53b2d5be3f1809e146
Pierre <32479816+Grompiler@users.noreply.github.com>
2023-07-13 19:10:56 +0200

    Update hop.lua

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -20,6 +20,6 @@ vim.keymap.set({ 'n', 'x' }, 's', function()
     hop.hint_char1({ current_line_only = false })
 end, { remap = true })
 
-vim.keymap.set({ 'n', 'x' }, '/', function()
+vim.keymap.set({ 'n', 'x' }, '?', function()
     hop.hint_patterns({ current_line_only = false })
 end, { remap = true })

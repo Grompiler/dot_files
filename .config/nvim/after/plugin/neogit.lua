fa13cb1b3d378839d3a605580078173a7385685e
Pierre <32479816+Grompiler@users.noreply.github.com>
2023-07-26 14:51:02 +0200

    Update neogit.lua

--- .config/nvim/after/plugin/neogit.lua
+++ .config/nvim/after/plugin/neogit.lua
@@ -1,6 +1,10 @@
 local neogit = require('neogit')
 
-neogit.setup {}
+neogit.setup {
+    integrations = {
+        telescope = true,
+        diffview = true,
+    },
+}
 
 vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<cr>')
-

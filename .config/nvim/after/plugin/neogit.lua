83310c5f38cba77752455024cda3a819efaa46c3
Pierre <32479816+Grompiler@users.noreply.github.com>
2023-07-26 23:11:17 +0200

    Update neogit.lua

--- .config/nvim/after/plugin/neogit.lua
+++ .config/nvim/after/plugin/neogit.lua
@@ -5,6 +5,12 @@ neogit.setup {
     integrations = {
         diffview = true,
     },
+    mappings = {
+        finder = {
+            ["<C-j>"] = "Next",
+            ["<C-k>"] = "Previous"
+        }
+    }
 }
 
 vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<cr>')

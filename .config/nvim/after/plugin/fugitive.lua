9eabbb4d86b9703a9f9e2c6cce9854751006c622
Pierre <32479816+Grompiler@users.noreply.github.com>
2024-01-09 00:16:36 +0100

    Update fugitive.lua

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -9,12 +9,14 @@ autocmd("FileType", {
         vim.keymap.set({ "n", "v" }, "<leader>f", "<cmd>Git fetch|echo '> git fetched'<cr>")
         vim.keymap.set({ "n", "v" }, "<leader>F", "<cmd>Git pull|echo '> git pulled'<cr>")
         vim.keymap.set({ "n", "v" }, "<leader>p", "<cmd>Git push|echo '> git pushed'<cr>")
-        vim.keymap.set({ "n", "v" }, "<leader>P", "<cmd>Git push --force|echo '> git forced push'<cr>")
-        vim.keymap.set({ "n", "v" }, "<leader>bb", "<cmd>Git branch -a|echo '> git branch all'<cr>")
-        vim.keymap.set({ "n", "v" }, "<leader>ll", "<cmd>Git log --graph|echo '> git log graph'<cr>")
+        -- vim.keymap.set({ "n", "v" }, "<leader>P", "<cmd>Git push --force|echo '> git forced push'<cr>")
+        vim.keymap.set({ "n", "v" }, "<leader>bl", "<cmd>Git branch --all --verbose|echo '> git branch all'<cr>")
+        vim.keymap.set({ "n", "v" }, "<leader>ll",
+            "<cmd>Git log --decorate --graph --name-only|echo '> git log graph'<cr>")
+        vim.keymap.set({ "n", "v" }, "<leader>la",
+            "<cmd>Git log --decorate --graph --all --name-only|echo '> git log all graph'<cr>")
         vim.keymap.set({ "n", "v" }, "<leader>zp", "<cmd>Git stash push|echo '> git stash push'<cr>")
         vim.keymap.set({ "n", "v" }, "<leader>zP", "<cmd>Git stash pop|echo '> git stash pop'<cr>")
         vim.keymap.set({ "n", "v" }, "<leader>zD", "<cmd>Git stash drop|echo '> git stash drop'<cr>")
-        vim.keymap.set({ "n", "v" }, "<leader>zl", "<cmd>Git stash list|echo '> git stash list'<cr>")
     end,
 })
